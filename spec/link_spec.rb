2eb3d5fa58b9c6b55973abb55475f0a239d24bc1
adil ali <adiln1993@gmail.com>
2016-03-22 12:50:07 +0000

    set up model and database

---
+++ spec/link_spec.rb
@@ -0,0 +1,8 @@
+# require 'link'
+#
+# describe Link do
+#   it '#self.create' do
+#     link = Link.create(title: 'gogo', url: 'www.monkeysforever.com')
+#     expect(Link.first).to eq link
+#   end
+# end

1aca73e4225984a0ddd139a5bbe3df45d51b7e75
Chris Parker <cjp214@gmail.com>
2016-03-22 13:48:00 +0000

    added databasecleaner gem

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,5 +1,6 @@
 feature 'View list of links' do
   scenario 'User sees a list of links on homepage' do
+    Link.create title: "Makers Academy", url: "www.makersacademy.com"
     visit '/links'
     expect(page.status_code).to eq 200
     within 'ul#links' do
